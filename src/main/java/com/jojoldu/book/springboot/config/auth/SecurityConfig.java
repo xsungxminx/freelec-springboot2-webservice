6359dcb7b81fa2024d52d789eb191ea02c0af2d0
xsungxminx <xsungxminx@naver.com>
2025-12-31 09:19:36 +0900

    git 20251231 시키류티 /payroll 은 oauth 안타게수정

--- src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()// csrf().disable().headers().frameOptions().disable() h2-console 화면을 사용하기 위해 해당 옵션들을 disable 합니다.
                 .and()
                 .authorizeRequests()// url 별 권한 관리를 설정하는 옵션의 시작점입니다. authorizeRequests가 선언되어야만 antMatchers 옵션을 사용할 수 있습니다.
-                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()  // 권한괸리 대상을 지정하는 옵션입니다. , URL/HTTP 메소드별로 관리가 가능합니다.
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile","/payroll/**").permitAll()  // 권한괸리 대상을 지정하는 옵션입니다. , URL/HTTP 메소드별로 관리가 가능합니다.
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name())  // "/"등 지정된 URL들은 permitAll() 옵션을 통해 전체 열람 권한을 주었습니다.  , /api/v1/** 주소를 가진 API는 USER권한을 가진 사람만 가능하도록 했습니다.
                 .anyRequest().authenticated()           // anyRequest 설정된 값들 이외 나머지 URL들을 나타냅니다. 여기서는authenticated()을 추가하여 나머지 URL들은 모두 인증된 사용자들에게만 허용하게 합니다.
                 .and()
